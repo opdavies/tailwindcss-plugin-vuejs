42f8585db84bf80f465604e1680d4ed25d6e91e3
Oliver Davies <oliver@microserve.io>
2018-12-15 22:02:33 +0000

    Add more options

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ module.exports = function (variants) {
   return function ({ addUtilities }) {
     addUtilities({
       '[v-cloak] .v-cloak-block': { display: 'block' },
+      '[v-cloak] .v-cloak-flex': { display: 'flex' },
+      '[v-cloak] .v-cloak-hidden': { display: 'none' },
       '[v-cloak] .v-cloak-inline': { display: 'inline' },
       '[v-cloak] .v-cloak-inline-block': { display: 'inline-block' },
-      '[v-cloak] .v-cloak-hidden': { display: 'none' },
+      '[v-cloak] .v-cloak-inline-flex': { display: 'inline-flex' },
       '[v-cloak] .v-cloak-invisible': { visibility: 'hidden' },
       '.v-cloak-block': { display: 'none' },
+      '.v-cloak-flex': { display: 'none' },
       '.v-cloak-inline': { display: 'none' },
+      '.v-cloak-inline-block': { display: 'none' },
+      '.v-cloak-inline-flex': { display: 'none' },
     }, variants)
   }
 }
