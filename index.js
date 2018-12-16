21888e6cab27690727b9d616b8e3afa5283f3b71
Oliver Davies <oliver@oliverdavies.uk>
2018-12-16 08:49:11 +0000

    Combine classes

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ module.exports = function (variants) {
       '[v-cloak] .v-cloak-inline-block': { display: 'inline-block' },
       '[v-cloak] .v-cloak-inline-flex': { display: 'inline-flex' },
       '[v-cloak] .v-cloak-invisible': { visibility: 'hidden' },
-      '.v-cloak-block': { display: 'none' },
-      '.v-cloak-flex': { display: 'none' },
-      '.v-cloak-inline': { display: 'none' },
-      '.v-cloak-inline-block': { display: 'none' },
-      '.v-cloak-inline-flex': { display: 'none' },
+      '.v-cloak-block, .v-cloak-flex, .v-cloak-inline, .v-cloak-inline-block, .v-cloak-inline-flex': { display: 'none' },
     }, variants)
   }
 }
