0cc092779c635761af4b79d855ba356461b3472c
Oliver Davies <oliver@oliverdavies.uk>
2019-04-12 00:06:42 +0100

    Use lodash, split tests

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-module.exports = function (variants) {
-  return function ({ addUtilities }) {
-    addUtilities({
-      '[v-cloak] .v-cloak-block': { display: 'block' },
-      '[v-cloak] .v-cloak-flex': { display: 'flex' },
-      '[v-cloak] .v-cloak-hidden': { display: 'none' },
-      '[v-cloak] .v-cloak-inline': { display: 'inline' },
-      '[v-cloak] .v-cloak-inline-block': { display: 'inline-block' },
-      '[v-cloak] .v-cloak-inline-flex': { display: 'inline-flex' },
-      '[v-cloak] .v-cloak-invisible': { visibility: 'hidden' },
-      '.v-cloak-block, .v-cloak-flex, .v-cloak-inline, .v-cloak-inline-block, .v-cloak-inline-flex': { display: 'none' },
-    }, variants)
+const _ = require('lodash')
+
+const defaultOptions = {
+  block: {
+    '[v-cloak] .v-cloak-block': { display: 'block' },
+    '.v-cloak-block': { display: 'none' }
+  },
+  flex: {
+    '[v-cloak] .v-cloak-flex': { display: 'flex' },
+    '.v-cloak-flex': { display: 'none' }
+  },
+  hidden: {
+    '[v-cloak] .v-cloak-hidden': { display: 'none' }
+  },
+  inline: {
+    '[v-cloak] .v-cloak-inline': { display: 'inline' },
+    '.v-cloak-inline': { display: 'none' }
+  },
+  'inline-block': {
+    '[v-cloak] .v-cloak-inline-block': { display: 'inline-block' },
+    '.v-cloak-inline-block': { display: 'none' }
+  },
+  'inline-flex': {
+    '[v-cloak] .v-cloak-inline-flex': { display: 'inline-flex' },
+    '.v-cloak-inline-flex': { display: 'none' }
+  },
+  invisible: {
+    '[v-cloak] .v-cloak-invisible': { visibility: 'hidden' }
   }
+}
+
+module.exports = ({
+  options = [],
+} = {}) => ({ addUtilities }) => {
+  const selectedOptions = !_.isEmpty(options)
+    ? options
+    : _.keys(defaultOptions)
+
+  addUtilities(_.flattenDeep(_.map(selectedOptions, option => {
+    return defaultOptions[option]
+  })))
 }
--- test.js
+++ test.js
@@ -11,11 +11,11 @@ const disableCorePlugins = () => {
   })
 }
 
-const generatePluginCss = () => {
+const generatePluginCss = (options = {}) => {
   return postcss(
     tailwindcss({
       corePlugins: disableCorePlugins(),
-      plugins: [plugin()],
+      plugins: [plugin(options)],
     })
   )
   .process('@tailwind utilities;', {
@@ -23,51 +23,99 @@ const generatePluginCss = () => {
   })
   .then(result => {
     return result.css;
-  });
+  })
 };
 
 expect.extend({
   toMatchCss: cssMatcher,
-});
+})
 
-test('it generates the default css for v-cloak classes', () => {
-  return generatePluginCss().then(css => {
+test('it generates block classes', () => {
+  return generatePluginCss({ options: ['block'] }).then(css => {
     expect(css).toMatchCss(`
       [v-cloak] .v-cloak-block {
         display: block;
       }
 
+      .v-cloak-block {
+        display: none;
+      }
+    `)
+  })
+})
+
+test('it generates hidden classes', () => {
+  return generatePluginCss({ options: ['hidden'] }).then(css => {
+    expect(css).toMatchCss(`
+      [v-cloak] .v-cloak-hidden {
+        display: none;
+      }
+    `)
+  })
+})
+
+test('it generates flex classes', () => {
+  return generatePluginCss({ options: ['flex'] }).then(css => {
+    expect(css).toMatchCss(`
       [v-cloak] .v-cloak-flex {
         display: flex;
       }
 
-      [v-cloak] .v-cloak-hidden {
+      .v-cloak-flex {
         display: none;
       }
+    `)
+  })
+})
 
+test('it generates inline classes', () => {
+  return generatePluginCss({ options: ['inline'] }).then(css => {
+    expect(css).toMatchCss(`
       [v-cloak] .v-cloak-inline {
         display: inline;
       }
 
+      .v-cloak-inline {
+        display: none;
+      }
+    `)
+  })
+})
+
+test('it generates inline block classes', () => {
+  return generatePluginCss({ options: ['inline-block'] }).then(css => {
+    expect(css).toMatchCss(`
       [v-cloak] .v-cloak-inline-block {
         display: inline-block;
       }
 
+      .v-cloak-inline-block {
+        display: none;
+      }
+    `)
+  })
+})
+
+test('it generates inline flex classes', () => {
+  return generatePluginCss({ options: ['inline-flex'] }).then(css => {
+    expect(css).toMatchCss(`
       [v-cloak] .v-cloak-inline-flex {
         display: inline-flex;
       }
 
-      [v-cloak] .v-cloak-invisible {
-        visibility: hidden;
-      }
-
-      .v-cloak-block,
-      .v-cloak-flex,
-      .v-cloak-inline,
-      .v-cloak-inline-block,
       .v-cloak-inline-flex {
         display: none;
       }
-    `);
-  });
-});
+    `)
+  })
+})
+
+test('it generates invisible classes', () => {
+  return generatePluginCss({ options: ['invisible'] }).then(css => {
+    expect(css).toMatchCss(`
+      [v-cloak] .v-cloak-invisible {
+        visibility: hidden;
+      }
+    `)
+  })
+})
