951f4b59359126cdf111dab87edaa63e615c5d9e
Oliver Davies <oliver@oliverdavies.uk>
2019-05-01 17:38:37 +0100

    Inline

--- test.js
+++ test.js
@@ -14,9 +14,7 @@ const generatePluginCss = (options = {}) => {
   .process('@tailwind utilities;', {
     from: undefined,
   })
-  .then(result => {
-    return result.css;
-  })
+  .then(result => result.css)
 };
 
 expect.extend({
