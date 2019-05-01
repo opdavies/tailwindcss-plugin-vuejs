1f4e76831ca191e8e09225f9045bfc4153b5a7b3
Oliver Davies <oliver@oliverdavies.uk>
2019-05-01 07:55:05 +0100

    Split onto separate lines

--- index.js
+++ index.js
@@ -2,30 +2,54 @@ const _ = require('lodash')
 
 const defaultOptions = {
   block: {
-    '[v-cloak] .v-cloak-block': { display: 'block' },
-    '.v-cloak-block': { display: 'none' }
+    '[v-cloak] .v-cloak-block': {
+      display: 'block'
+    },
+    '.v-cloak-block': {
+      display: 'none'
+    }
   },
   flex: {
-    '[v-cloak] .v-cloak-flex': { display: 'flex' },
-    '.v-cloak-flex': { display: 'none' }
+    '[v-cloak] .v-cloak-flex': {
+      display: 'flex'
+    },
+    '.v-cloak-flex': {
+      display: 'none'
+    }
   },
   hidden: {
-    '[v-cloak] .v-cloak-hidden': { display: 'none' }
+    '[v-cloak] .v-cloak-hidden': {
+      display: 'none'
+    }
   },
   inline: {
-    '[v-cloak] .v-cloak-inline': { display: 'inline' },
-    '.v-cloak-inline': { display: 'none' }
+    '[v-cloak] .v-cloak-inline': {
+      display: 'inline'
+    },
+    '.v-cloak-inline': {
+      display: 'none'
+    }
   },
   'inline-block': {
-    '[v-cloak] .v-cloak-inline-block': { display: 'inline-block' },
-    '.v-cloak-inline-block': { display: 'none' }
+    '[v-cloak] .v-cloak-inline-block': {
+      display: 'inline-block'
+    },
+    '.v-cloak-inline-block': {
+      display: 'none'
+    }
   },
   'inline-flex': {
-    '[v-cloak] .v-cloak-inline-flex': { display: 'inline-flex' },
-    '.v-cloak-inline-flex': { display: 'none' }
+    '[v-cloak] .v-cloak-inline-flex': {
+      display: 'inline-flex'
+    },
+    '.v-cloak-inline-flex': {
+      display: 'none'
+    }
   },
   invisible: {
-    '[v-cloak] .v-cloak-invisible': { visibility: 'hidden' }
+    '[v-cloak] .v-cloak-invisible': {
+      visibility: 'hidden'
+    }
   }
 }
 
