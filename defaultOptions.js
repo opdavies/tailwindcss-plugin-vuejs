19cc7caf310f2f4580cb587f07c7b207de44f7e5
Oliver Davies <oliver@oliverdavies.uk>
2019-05-01 08:03:04 +0100

    Move defaultOptions into their own file

---
+++ defaultOptions.js
@@ -0,0 +1,58 @@
+module.exports = {
+  block: {
+    '[v-cloak] .v-cloak-block': {
+      display: 'block'
+    },
+    '.v-cloak-block': {
+      display: 'none'
+    }
+  },
+
+  flex: {
+    '[v-cloak] .v-cloak-flex': {
+      display: 'flex'
+    },
+    '.v-cloak-flex': {
+      display: 'none'
+    }
+  },
+
+  hidden: {
+    '[v-cloak] .v-cloak-hidden': {
+      display: 'none'
+    }
+  },
+
+  inline: {
+    '[v-cloak] .v-cloak-inline': {
+      display: 'inline'
+    },
+    '.v-cloak-inline': {
+      display: 'none'
+    }
+  },
+
+  'inline-block': {
+    '[v-cloak] .v-cloak-inline-block': {
+      display: 'inline-block'
+    },
+    '.v-cloak-inline-block': {
+      display: 'none'
+    }
+  },
+
+  'inline-flex': {
+    '[v-cloak] .v-cloak-inline-flex': {
+      display: 'inline-flex'
+    },
+    '.v-cloak-inline-flex': {
+      display: 'none'
+    }
+  },
+
+  invisible: {
+    '[v-cloak] .v-cloak-invisible': {
+      visibility: 'hidden'
+    }
+  }
+}
